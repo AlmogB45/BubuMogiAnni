14c0b69407a74b9ca344550c11191a32dab2a0c3
Almog Ben Yamin <almmogx@gmail.com>
2024-11-22 02:19:49 +0200

    Second Commit

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -10,7 +10,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDCkMNobdZeIxhm81P12OH60wdguVtIrQs",
+  apiKey: REACT_APP_FIREBASE_API_KEY,
   authDomain: "bubumogianni-81cb9.firebaseapp.com",
   projectId: "bubumogianni-81cb9",
   storageBucket: "bubumogianni-81cb9.firebasestorage.app",
